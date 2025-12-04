3c1afbf9e514fe9aa58e503c820dfbe39b27231b
Ngalaka <ngalakagift@gmail.com>
2025-12-04 11:47:38 +0100

    i update code

---
+++ script.js
@@ -0,0 +1,9 @@
+let links = document.getElementById("links")
+  links.style.maxHeight ="0px"
+  function toggle(){
+    if(links.style.maxHeight ="0px"){
+      links.style.maxHeight ="400px"
+    }else{
+      links.style.maxHeight ="0px"
+    }
+  }
